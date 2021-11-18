4677eb8a5acb2be565ee58cc1915dc36834f7a8d
alexsherba29 <alexsherba29@gmail.com>
2021-11-18 19:28:19 +0200

    add trainer

---
+++ trainer.py
@@ -0,0 +1,24 @@
+from tensorflow.keras.preprocessing import ImageDataGenerator
+from tensorflow.keras.applications import InceptionV3
+from tensorflow.keras.layers import Dropout, Flatten, Dense, Input
+from tensorlfow.keras.models import Model
+from tensorflow.keras.optimizers import SGD, Adam
+
+
+
+def build_model(nbr_classes):
+
+    base_model = InceptionV3(weights="imagenet", include_top=False, input_tensor=Input(shape=(224, 224, 3)))
+
+    head_model = base_model.output
+    head_model = Flatten()(head_model)
+    head_model = Dense(512)(head_model)
+    head_model = Dropout(0.5)(head_model)
+    head_model = Dense(nbr_classes, activation="softmax")(head_model)
+
+    model = Model(inputs=base_model.input, outputs=head_model)
+
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    return model
